0a8501b108060e2e1de9d2192ebed6fd4821ddfb
Nicolas Vycas Nery <vycasnicolas@gmail.com>
2020-11-24 15:03:07 -0300

    Adicionado um cometario💬

--- RunEditor.bat
+++ RunEditor.bat
@@ -1,2 +1,3 @@
-start chrome 127.0.0.1:8050/Blogeditor.php
+//start chrome --app="http://127.0.0.1:8050/Blogeditor.php"
+start chrome "http://127.0.0.1:8050/Blogeditor.php"    
 php -S 127.0.0.1:8050
